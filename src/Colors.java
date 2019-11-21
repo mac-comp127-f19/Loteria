c8804c09344cf422c8f6c8eb4fffa7ae94b7404b
ivycontreras <ivycontreras88@gmail.com>
2019-11-21 15:16:38 -0600

    Loteria

---
+++ src/Colors.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Colors {
+
+    public Colors(){
+        List<Color> colors = Arrays.asList(
+        new Color(255, 0, 0),
+        new Color(0, 255, 0),
+        new Color(0, 0, 255),
+        new Color(255, 249, 0),
+        new Color(255, 111, 0),
+        new Color(143, 0, 255),
+        new Color(0, 254, 255),
+        new Color(153, 255, 188),
+        new Color(255, 182, 227),
+        new Color(255, 210, 168),
+        new Color(183, 177, 255),
+        new Color(255, 88, 234),
+        new Color(0,0,0),
+        new Color(255, 255, 255),
+        new Color(90, 90, 90),
+        new Color(65, 47, 35),
+        new Color(3, 55, 19),
+        new Color(64,0, 5));
+    }
+}
---
+++ src/MainCard.java
@@ -0,0 +1,2 @@
+public class MainCard {
+}
