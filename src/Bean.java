a08eff85165878feba38b758c34abe03f40be52c
Haley Vien <54863151+cloudhaley@users.noreply.github.com>
2019-11-24 19:28:31 -0600

    Removed checkCard method

--- src/Bean.java
+++ src/Bean.java
@@ -1,11 +1,14 @@
 import comp127graphics.Ellipse;
 
+import java.awt.*;
+
 public class Bean extends Ellipse {
     private static final double BEAN_RADIUS = 6;
 
     public Bean(double x, double y) {
         super(x, y, 4 * BEAN_RADIUS, 2 * BEAN_RADIUS);
-
+        setFillColor(Color.black);
+        setFilled(true);
     }
 
 }
