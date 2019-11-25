637eee8f843efb3469785ffcbeb7a5fff090afff
ivycontreras <ivycontreras88@gmail.com>
2019-11-24 18:33:06 -0600

    Hw 4 - BreakOut Game

--- src/LoteriaGame.java
+++ src/LoteriaGame.java
@@ -1,3 +1,19 @@
-public class LoteriaGame {
+import comp127graphics.CanvasWindow;
 
+public class LoteriaGame {
+    private static final int CANVAS_WIDTH = 800;
+    private static final int CANVAS_HEIGHT = 600;
+    private MainCard mainCard;
+
+    private CanvasWindow canvas;
+
+    public LoteriaGame() {
+        canvas = new CanvasWindow("Breakout!", CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        mainCard = new MainCard(canvas);
+    }
+
+    public static void main(String[] args){
+        new LoteriaGame();
+    }
 }
