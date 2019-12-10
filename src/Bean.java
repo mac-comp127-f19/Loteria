17b9525c310fddf36562b23ff8f7373b6d2e5865
ivycontreras <ivycontreras88@gmail.com>
2019-12-10 17:16:26 -0600

    Loteria

--- src/Bean.java
+++ src/Bean.java
@@ -1,9 +1,6 @@
-import comp127graphics.CanvasWindow;
 import comp127graphics.Image;
 
 public class Bean extends Image {
-    private static final double BEAN_RADIUS = 6;
-    private CanvasWindow canvas;
     private String beanImage = "bean.png";
     private double x;
     private double y;
